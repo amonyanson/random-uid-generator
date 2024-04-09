439cc557aa4ab0b3aab25da3b7e8d9f6ee23ed96
AFT3RM@TH <amonianson@gmail.com>
2024-04-09 22:32:32 +0300

    initial commit

--- random-uid.py
+++ random-uid.py
@@ -1,8 +1,19 @@
-import random 
+import random
+
 import string
 
-def random_uid():
+import time
+
+def random_uid(): 
+	num_characters = int(input('Enter number of characters:\n')) 
+	time.sleep(0.5)
+	num_ids = int(input('Enter number of IDS:\n'))
+	time.sleep(0.5)
 	pool = string.ascii_uppercase + string.digits
-	random_id = ''.join(random.choice(pool) for _ in range(6))
-	return random_id
-print(random_uid())
+	random_ids = [''.join(random.choice(pool) for _ in range(num_characters)) for _ in range(num_ids)]
+	for key in random_ids:
+		time.sleep(1)
+		print(key)
+		time.sleep(1)
+		print("Successfully done")
+random_uid()
